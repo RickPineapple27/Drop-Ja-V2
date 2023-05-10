5ffead8c4ecd29bbde768ae2507fdfcdd167f69e
RickPineapple27 <107954848+RickPineapple27@users.noreply.github.com>
2023-05-10 14:04:55 -0600

    IncorporacionId

--- Assets/Scripts/Inventory/Objects.cs
+++ Assets/Scripts/Inventory/Objects.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Object", menuName = "Inventory Objects/Create New")]
 public class Objects : ScriptableObject
 {
+    public int Id;
     public string itemName;
     public Sprite itemSprite;
     public bool stackable = true;
